ee29a32c6976d6b2b58eb136bcd0155dcedadaa2
Lorenzo Mazzarotto <mazzsva@icloud.com>
2026-07-04 16:49:26 +0200

    Initial Commit

---
+++ Calliope/CalliopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalliopeApp.swift
+//  Calliope
+//
+//  Created by Lorenzo Mazzarotto on 04/07/26.
+//
+
+import SwiftUI
+
+@main
+struct CalliopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
